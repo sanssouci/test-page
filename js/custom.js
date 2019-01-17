4e3d27fb7832fa6c8c546131b72f8c74d2817ae6
Sidra Rubel <sidra@wondermentapps.com>
2019-01-17 18:47:04 -0500

    FINAL

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,35 @@
 jQuery(function() {
 
-	// Toggle mobile menu active on click
-	jQuery('#menu-toggle').click(function(){
-		jQuery(this).find("i").toggleClass('fa-times');
-		jQuery(this).next("#mobile-menu").fadeToggle("fast");
-	});
+    // Toggle mobile menu active on click
+    jQuery('#menu-toggle').click(function() {
+        jQuery(this).find("i").toggleClass('fa-times');
+        jQuery(this).next("#mobile-menu").fadeToggle("fast");
+    });
 
-	// Set form password visibility 
-	jQuery(".toggle-password").click(function() {
-		jQuery(this).toggleClass("fa-eye fa-eye-slash");
-		var input = jQuery(jQuery(this).attr("toggle"));
-		if (input.attr("type") == "password") {
-		input.attr("type", "text");
-		} else {
-		input.attr("type", "password");
-		}
-	});
+    // Set form password visibility 
+    jQuery(".toggle-password").click(function() {
+        jQuery(this).toggleClass("fa-eye fa-eye-slash");
+        var input = jQuery(jQuery(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
 
-	// Delay dummy form submit to show loading
-	jQuery("#signup").on('submit', function(e) {
-	    e.preventDefault();
+    // Delay dummy form submit to show loading
+    jQuery("#signup").on('submit', function(e) {
+        e.preventDefault();
 
-	    jQuery(".submit i").addClass("loading fa-spinner fa-spin");
+        jQuery(".submit i").addClass("loading fa-spinner fa-spin");
 
-	     setTimeout(function() {
-	      jQuery(".submit i").removeClass("loading fa-spinner fa-spin");
-	     /*document.getElementById("signup").reset();*/
-	     jQuery("#signup input, #signup button, #signup i, #signup .terms").css('opacity','0'); 
-	     jQuery("#signup .success-message").fadeIn(); 
-	    }, 3000);
+        setTimeout(function() {
+            jQuery(".submit i").removeClass("loading fa-spinner fa-spin");
+            /*document.getElementById("signup").reset();*/
+            jQuery("#signup input, #signup button, #signup i, #signup .terms").css('opacity', '0');
+            jQuery("#signup .success-message").fadeIn();
+        }, 3000);
 
-	});
+    });
 
 });
